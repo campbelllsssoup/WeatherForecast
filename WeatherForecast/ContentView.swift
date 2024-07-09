0cbb524854e9f955cdd15849a6310381ed399638
Matthew Campbell <matjocampbell@gmail.com>
2024-07-09 14:11:29 -0600

    Adds cards for the weather forecast

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,16 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(day: "Mon", high: 76, low: 58, isRainy: false)
+            DayForecast(day: "Tue", high: 67, low: 45, isRainy: true)
+//            DayForecast(day: "Wed", high: 72, low: 60, isRainy: false)
+//            DayForecast(day: "Thu", high: 78, low: 65, isRainy: true)
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let high: Int
+    let low: Int
+    let isRainy: Bool
+    var iconName: String { isRainy ? "cloud.rain.fill" : "sun.max.fill" }
+    var iconColor: Color { isRainy ? Color.blue : Color.yellow }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)")
+                .fontWeight(Font.Weight.ultraLight)
+            Text("Low: \(low)")
+                .fontWeight(Font.Weight.ultraLight)
+                .foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
 }
