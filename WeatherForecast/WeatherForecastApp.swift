18a72b6290a66105090251475c9c58665d1d833e
Matthew Campbell <matjocampbell@gmail.com>
2024-07-09 13:15:45 -0600

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Matt Campbell on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
